d7d0cdad0bbd4bfa3073d5809906ae25340c372f
Houyi Chen <vchen4@andrew.cmu.edu>
2025-07-09 17:28:21 +0800

    basic structure for login and registration

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
